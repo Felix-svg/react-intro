42b699f64c16abd3d47b28a43b0a055bdc34edc8
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-28 22:48:49 +0300

    Created an About component

---
+++ my-react-app/src/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return(
+    <div>
+      <h2>About Us</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium minus atque ullam, assumenda saepe tempore autem, consectetur error consequatur dolorum, ea officiis temporibus! Error iste doloremque dolorem ex consequatur nulla. Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aspernatur officia atque sint provident! Autem consequatur aperiam saepe, architecto, quam rem ab incidunt neque eveniet repellendus impedit atque recusandae fugit.</p>
+    </div>
+  )
+}
+
+export default About
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer"
+import About from "./About"
 
 
 function App() {
   return(
     <>
     <Header />
+    <About />
     <Footer />
     </>
   )
