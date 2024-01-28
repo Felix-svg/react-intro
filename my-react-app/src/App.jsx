7a0862ca5c9455cbf623e5a3af7c6229e833406a
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-28 23:03:38 +0300

    Created a Contact component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./Footer"
 import About from "./About"
 import Projects from "./Projects"
 import Codes from "./Codes"
+import Contact from "./Contact"
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Projects />
     <Codes />
     <Footer />
+    <Contact />
     </>
   )
 }
---
+++ my-react-app/src/Contact.jsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return(
+    <div className="icons">
+    <a href="https://x.com/felixomondi_" target="_blank" rel="noreferrer"><i className="fa-brands fa-x-twitter"></i></a>
+    <a href="https://instagram.com/_omondi_" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
+    <a href="https://github.com/Felix-svg" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a>
+    <a href="https://www.linkedin.com/in/felix-omondi=b13b53232" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+</div>
+  )
+}
+
+export default Contact
